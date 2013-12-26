570f78ff3975fc2d1012414be81a41b3c2dd000d
Gerhut <Gerhut@GMail.com>
2013-12-26 19:18:28 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('#to-index').on('click', function () {
+    $.scrollTo('header', 300);
+  });
+  $('#to-knowledge').on('click', function () {
+    $.scrollTo('#knowledge', 300);
+  });
+});
