3267d2dca62b3df4e5a7068a918e4a48f984dcf7
Gerhut <Gerhut@GMail.com>
2013-12-31 19:24:22 +0800

    2013-12-31 Commit

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
 $(function () {
-  $('#to-index').on('click', function () {
-    $.scrollTo('header', 300);
-  });
-  $('#to-knowledge').on('click', function () {
-    $.scrollTo('#knowledge', 300);
-  });
 });
