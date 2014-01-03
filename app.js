538ff973b9240f3d38ceb39d1bf2c7ed1d46c5e8
Gerhut <Gerhut@GMail.com>
2014-01-03 11:06:41 +0800

    Update dynamic layout

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 $(function () {
+  var $body = $('body')
+    , $header = $('header')
+    , $carouselItem = $('.carousel .item');
+  function dynamicLayout() {
+    $body.css('margin-top', $header.height());
+    $header.css('margin-left', -$header.outerWidth() / 2 + 'px');
+    $carouselItem.height($body.width() / 2);
+  }
+  $(dynamicLayout);
+  $(window).on('resize', dynamicLayout);
 });
